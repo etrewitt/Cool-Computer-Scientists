f758ffc5d0fe89450a97744514b2310f5c5b0089
Erik <etrewitt@users.noreply.github.com>
2015-02-18 22:42:31 -0800

    First upload
    Let's get this party started.

---
+++ Cool-Computer-Scientists/Cool-Computer-Scientists/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cool-Computer-Scientists
+//
+//  Created by Erik Trewitt on 2/18/15.
+//  Copyright (c) 2015 etrewitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
